471e76ea17edb11ac9ab07f400f83a1d86749dde
Paul Visscher <paulv@canonical.org>
2021-07-25 05:31:00 -0400

    prototype for mapping keypresses to commands. not sure if I like this, hard to modify/extend.

---
+++ commands.rb
@@ -0,0 +1,32 @@
+class Commands
+  def initialize
+  end
+
+  def kp0
+    puts "fire kp0"
+  end
+
+  def kp0
+    puts "fire kp0"
+  end
+
+end
+
+
+# numlock
+# backspace
+# kpslash
+# kpasterisk
+# kpminus
+# kpplus
+# kpenter
+# kpdot
+# kp7
+# kp8
+# kp9
+# kp4
+# kp5
+# kp6
+# kp1
+# kp2
+# kp3
--- keypad-read.rb
+++ keypad-read.rb
@@ -1,24 +1,26 @@
 require 'device_input'
 require 'pry'
 require './ffi.rb'
+require './commands.rb'
 
 DEVICE = "/dev/input/by-id/usb-04d9_1203-event-kbd"
 
 device = File.open(DEVICE, "r")
 device.ioctl(EVIOCGRAB, 1)
 
-puts DeviceInput::Event::BYTE_LENGTH
-
-io = device
+commands = Commands.new
 
 loop {
-  bytes = io.read(DeviceInput::Event::BYTE_LENGTH)
+  bytes = device.read(DeviceInput::Event::BYTE_LENGTH)
   break unless (bytes and bytes.length == DeviceInput::Event::BYTE_LENGTH)
 
   decoded_data = DeviceInput::Event.decode(bytes)
   data = DeviceInput::Event.new(decoded_data)
   if data.type == "EV_KEY"
-    puts "keyup on #{data.code}" if data.value == 0
-    puts "keydown on #{data.code}" if data.value == 1
+    begin
+      commands.send(data.code.downcase) if data.value == 0
+    rescue NoMethodError => e
+      puts "No method named #{data.code.downcase} yet"
+    end
   end
 }
