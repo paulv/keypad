91111f455aea6c1883860e61f5db7f99ca2c345c
Paul Visscher <paulv@canonical.org>
2021-08-03 05:09:36 -0400

    map keypad keys to functions

--- keypad-read.rb
+++ keypad-read.rb
@@ -1,9 +1,12 @@
 require 'device_input'
 require 'pry'
+require 'yaml'
 require './ffi.rb'
 require './commands.rb'
 
+
 DEVICE = "/dev/input/by-id/usb-04d9_1203-event-kbd"
+command_map = YAML.load_file "config.yaml"
 
 device = File.open(DEVICE, "r")
 device.ioctl(EVIOCGRAB, 1)
@@ -18,7 +21,10 @@ loop {
   data = DeviceInput::Event.new(decoded_data)
   if data.type == "EV_KEY"
     begin
-      commands.send(data.code.downcase) if data.value == 0
+      if data.value == 0
+        command = command_map[data.code.downcase]
+        commands.send(command)
+      end
     rescue NoMethodError => e
       puts "No method named #{data.code.downcase} yet"
     end
