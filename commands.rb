ccce6d77ba0ab4cbf239d04a78cfb9b6b156e3a5
Paul Visscher <paulv@canonical.org>
2021-08-09 20:32:44 -0400

    mute, volume up/down

--- commands.rb
+++ commands.rb
@@ -6,22 +6,30 @@ class Commands
 
   def switch_to_ps4
     exec_command("input-selector=dvd")
-    # onkyo --host 192.168.11.21 --port 60128 input-selector=dvd
   end
 
   def switch_to_ps5
     exec_command("input-selector=video2")
-    # onkyo --host 192.168.11.21 --port 60128 input-selector=video2
   end
 
   def switch_to_pc
     exec_command("input-selector=strm-box")
-    # onkyo --host 192.168.11.21 --port 60128 input-selector=strm-box
   end
 
   def switch_to_switch
     exec_command("input-selector=game")
-    # onkyo --host 192.168.11.21 --port 60128 input-selector=game
+  end
+
+  def toggle_mute
+    exec_command("audio-muting=toggle")
+  end
+
+  def volume_up
+    exec_command("master-volume=level-up")
+  end
+
+  def volume_down
+    exec_command("master-volume=level-down")
   end
 
   def exec_command(command)
